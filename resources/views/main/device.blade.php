646f1baed8d99c41aef5744d0202027006adb5b3
Hafijur <bmsakib09.hr@gmail.com>
2021-06-28 18:29:59 +0600

    gradient coler

--- resources/views/main/device.blade.php
+++ resources/views/main/device.blade.php
@@ -27,21 +27,51 @@
             <div class="container">
 <style>
                 .detail-header {
-                    background: linear-gradient(90deg, #3b3a3a91, #444444d7, #444444d7, #3b3a3a85, #d88c77, #dc5c39de, #dc5c39de, #c53007b7, #ca2d01e7);
+                    background: rgba(0,0,0,.2);
+                    /* background: linear-gradient(90deg, #3b3a3a91, #444444d7, #444444d7, #3b3a3a85, #d88c77, #dc5c39de, #dc5c39de, #c53007b7, #ca2d01e7); */
                     border-bottom: #44444473 solid 1px;
 
                 }
 
                 .details-inner {
-                    background: linear-gradient(90deg, #ffffff, #ffffff, #ececec, #aaaaaa8c, #7a7a7a93, #d88c77, #df6442de, #e46d4ce3, #ee8a6f, #f38567e3, #f3b2a0);
+                    /* background: linear-gradient(90deg, #ffffff, #ffffff, #ececec, #aaaaaa8c, #7a7a7a93, #d88c77, #df6442de, #e46d4ce3, #ee8a6f, #f38567e3, #f3b2a0); */
                     border-bottom: #44444467 solid 1px;
                 }
 
                 .details-footer {
-                    background: linear-gradient(90deg, #3b3a3a91, #444444d7, #444444d7, #3b3a3a85, #d88c77, #dc5c39de, #dc5c39de, #c53007b7, #ca2d01e7);
+                    background: rgba(0,0,0,.2);
+                    /* background: linear-gradient(90deg, #3b3a3a91, #444444d7, #444444d7, #3b3a3a85, #d88c77, #dc5c39de, #dc5c39de, #c53007b7, #ca2d01e7); */
                     padding: 7px;
                 }
-
+                /* .headerBackground{
+                    overflow: hidden;
+                    position: relative;
+                }
+                .headerBackground::before{
+                    background-image: url("{{asset($device->image)}}");
+                    background-size: 500%;
+                    background-position: -100px -1200px;
+                    filter: blur(30px);
+                    -webkit-filter: blur(30px);
+                    content: "";
+                    position: absolute;
+                    z-index: 0;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                } */
+                .headerBackground{
+                    overflow: hidden;
+                    position: relative;
+                    width: 100%;
+                    height: 100%;
+                    background:linear-gradient(90deg,#ffffff,rgb(82,81,85),rgb(218,218,218))
+                }
+                .headerBackground::before{
+                    filter: blur(30px);
+                    -webkit-filter: blur(30px);
+                }
                 .details-footer a {
                     color: aliceblue;
                     padding: 8px 20px;
@@ -52,101 +82,106 @@
                 .details-footer a:hover {
                     background-color: red;
                 }
+                .heder-image{
+                    background: #ffffff;
+                }
             </style>
 
 <!-- mobile top feature -->
-            <div class="row details p-0">
-                <div class="col-sm-12 detail-header">
-                    <div class="row p-3">
-                        <div class="col-sm-6">
-                            <h4 class="text-white font-weight-bold">{{$device->brand->title}} {{$device->title}}</h4>
-                        </div>
-                        <div class="col-sm-6 text-right">
-                            <h4><i class="fa fa-share-alt text-white font-weight-light"
-                                    aria-hidden="true"></i></h4>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-12 details-inner">
-                    <div class="row">
-                        <div class="col-sm-2 p-2">
-                            <!-- mobile pic  -->
-                            {{-- <img src="{{asset($device->image)}}" alt="{{$device->brand->title}} {{$device->title}}"> --}}
-                            <img src="{{asset($device->image)}}" alt="">
-                            {{-- <img src="{{asset("/storage/device_images/".strtolower($device->slug).".jpg")}}" alt="{{$device->brand->title}} {{$device->title}}"> --}}
-                        </div>
-                        <div class="col-sm-10">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="row text-dark">
-                                        <div class="col-sm-12 pt-3 border-right">
-                                            @foreach ($sps_header_1 as $sh_1)
-                                            @php
-                                                // dd($sp_header_1['icon']);
-                                                $sh1keys=array_keys($sh_1);
-                                                $sh1values=array_values($sh_1);
-                                                if(strlen($sh1values[1])<3){
-                                                    continue;
-                                                }
-                                            @endphp
-                                            <p style="line-height: 0;" title="{{$sh1keys[1]}}">
-                                                <i class="icofont-{{$sh1values[0]}} icofont-1x"></i> {{$sh1values[1]}}
-                                            </p>
-                                            @endforeach
-                                        </div>
-                                        
-                                    </div>
-                                </div>
-                                <div class="col-sm-3">
-                                    <div class="row">
-                                        <div class="col-sm-12 border-right pt-3 text-center">
-                                            <h3>N/A</h3>
-                                            <h6>1505 Hits</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-3">
-                                    <div class="row">
-                                        <div class="col-sm-12 border-right pt-3 text-center">
-                                            <h3>3</h3>
-                                            <h6>BECOME A FAN</h6>
-                                        </div>
-                                    </div>
-                                </div>
+            <div class="headerBackground">
+                <div class="row details p-0">
+                    <div class="col-sm-12 detail-header">
+                        <div class="row p-3">
+                            <div class="col-sm-6">
+                                <h4 class="text-white font-weight-bold">{{$device->brand->title}} {{$device->title}}</h4>
                             </div>
-                            <div class="row">
-                                @foreach ($sps_header_2 as $sh_2)
-                                @php
-                                    // dd($sp_header_1['icon']);
-                                    $sh2keys=array_keys($sh_2);
-                                    $sh2values=array_values($sh_2);
-                                    if(strlen($sh2values[1])<3 && strlen($sh2values[2])<3){
-                                        continue;
-                                    }
-                                @endphp
-                                <div class="col-sm-3 border-right mt-5">
-                                    <h5><i class="icofont-{{$sh2values[0]}} icofont-2x"></i></h5>
-                                    <h3>{{$sh2values[1]}}</h3>
-                                    <h6 class="mb-2">{{$sh2values[2]}}</h6>
-                                </div>
-                                @endforeach
+                            <div class="col-sm-6 text-right">
+                                <h4><i class="fa fa-share-alt text-white font-weight-light"
+                                        aria-hidden="true"></i></h4>
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="col-sm-12 details-footer">
-                    <div class="row">
-                        <div class="col-sm-6"></div>
-                        <div class="col-sm-2">
-                            <a href="#">Options</a>
+    
+                    <div class="col-sm-12 details-inner">
+                        <div class="row">
+                            <div class="col-sm-2 p-2 heder-image">
+                                <!-- mobile pic  -->
+                                {{-- <img src="{{asset($device->image)}}" alt="{{$device->brand->title}} {{$device->title}}"> --}}
+                                <img id="mainImage" src="{{asset($device->image)}}" alt="">
+                                {{-- <img src="{{asset("/storage/device_images/".strtolower($device->slug).".jpg")}}" alt="{{$device->brand->title}} {{$device->title}}"> --}}
+                            </div>
+                            <div class="col-sm-10 headerBackground">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="row text-dark">
+                                            <div class="col-sm-12 pt-3 border-right">
+                                                @foreach ($sps_header_1 as $sh_1)
+                                                @php
+                                                    // dd($sp_header_1['icon']);
+                                                    $sh1keys=array_keys($sh_1);
+                                                    $sh1values=array_values($sh_1);
+                                                    if(strlen($sh1values[1])<3){
+                                                        continue;
+                                                    }
+                                                @endphp
+                                                <p style="line-height: 0;" title="{{$sh1keys[1]}}">
+                                                    <i class="icofont-{{$sh1values[0]}} icofont-1x"></i> {{$sh1values[1]}}
+                                                </p>
+                                                @endforeach
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <div class="row">
+                                            <div class="col-sm-12 border-right pt-3 text-center">
+                                                <h3>N/A</h3>
+                                                <h6>1505 Hits</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <div class="row">
+                                            <div class="col-sm-12 border-right pt-3 text-center">
+                                                <h3>3</h3>
+                                                <h6>BECOME A FAN</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    @foreach ($sps_header_2 as $sh_2)
+                                    @php
+                                        // dd($sp_header_1['icon']);
+                                        $sh2keys=array_keys($sh_2);
+                                        $sh2values=array_values($sh_2);
+                                        if(strlen($sh2values[1])<3 && strlen($sh2values[2])<3){
+                                            continue;
+                                        }
+                                    @endphp
+                                    <div class="col-sm-3 border-right mt-5">
+                                        <h5><i class="icofont-{{$sh2values[0]}} icofont-2x"></i></h5>
+                                        <h3>{{$sh2values[1]}}</h3>
+                                        <h6 class="mb-2">{{$sh2values[2]}}</h6>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-sm-2">
-                            <a href="#">compare</a>
-                        </div>
-                        <div class="col-sm-2">
-                            <a href="#">pictures</a>
+                    </div>
+    
+                    <div class="col-sm-12 details-footer">
+                        <div class="row">
+                            <div class="col-sm-6"></div>
+                            <div class="col-sm-2">
+                                <a href="#">Options</a>
+                            </div>
+                            <div class="col-sm-2">
+                                <a href="#">compare</a>
+                            </div>
+                            <div class="col-sm-2">
+                                <a href="#">pictures</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -426,4 +461,42 @@
 @endsection
 @push('head')
 <link rel="stylesheet" href="{{asset('assets/icofont/icofont.min.css')}}">
+@endpush
+@push('script')
+<script src="{{asset('assets/js/color-thief.js')}}"></script>
+<script>
+    function applyColor(color){
+        var grad="linear-gradient(90deg,#ffffff,rgb("+color[0]+","+color[1]+","+color[2]+"))";
+        console.log(grad);
+        jQuery(".headerBackground").css('background',grad);
+    }
+    function applyColorFromPalette(pal){
+        var cols=[];
+        for(color of pal){
+            cols.push("rgb("+color.join()+")");
+        }
+        var grad="linear-gradient(60deg,#ffffff,"+cols.join()+")";
+        console.log(grad);
+        jQuery(".headerBackground").css('background',grad);
+    }
+    const colorThief = new ColorThief();
+    const img = document.querySelector('#mainImage');
+
+    // Make sure image is finished loading
+    if (img.complete) {
+    //  const col= colorThief.getColor(img);
+     const pal= colorThief.getPalette(img,2,1);
+     console.log(pal);
+     applyColorFromPalette(pal);
+    //  applyColor(col);
+    } else {
+      img.addEventListener('load', function() {
+        // const col= colorThief.getColor(img);
+        const pal= colorThief.getPalette(img,2,1);
+        console.log(pal);
+        applyColorFromPalette(pal);
+        // applyColor(col);
+      });
+    }
+</script>
 @endpush
